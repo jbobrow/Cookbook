05f8ba0fe1b4300ceda00c15b9169a63fe3951cc
Jonathan Bobrow <jbobrow@gmail.com>
2026-01-04 17:32:24 -0500

    Project files for first version of app

--- Cookbook/CookbookApp.swift
+++ Cookbook/CookbookApp.swift
@@ -9,9 +9,22 @@ import SwiftUI
 
 @main
 struct CookbookApp: App {
+    @StateObject private var recipeStore = RecipeStore()
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            RecipeListView()
+                .environmentObject(recipeStore)
         }
+        #if os(macOS)
+        .commands {
+            CommandGroup(replacing: .newItem) {
+                Button("New Recipe") {
+                    // This would need to be handled via a published property in RecipeStore
+                }
+                .keyboardShortcut("n", modifiers: .command)
+            }
+        }
+        #endif
     }
 }
