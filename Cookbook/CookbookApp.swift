33a84f2b8e490cf6f6682d70ed05e570306b90f6
Jonathan Bobrow <jbobrow@gmail.com>
2026-01-04 16:23:36 -0500

    Initial Commit

---
+++ Cookbook/CookbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookbookApp.swift
+//  Cookbook
+//
+//  Created by Jonathan Bobrow on 1/4/26.
+//
+
+import SwiftUI
+
+@main
+struct CookbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
